9b01e8ea3f05e5f61212581052533554f82c83fc
ultra-u <2137978825@qq.com>
2025-04-27 19:05:20 +0800

    Decode_string

---
+++ Decode_string/Solution.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    string decodeString(string s) {
+            
+    }
+};
+
+int main(){
+    string s;
+    cout<<"please enter string:\n";
+    getline(cin,s);
+    Solution s1;
+    cout<<"decode: "<<s1.decodeString(s);
+}
